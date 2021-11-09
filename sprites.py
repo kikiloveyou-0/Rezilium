b61af2af03361735436e3e0b8af851af8fdb81e2
kikiloveyou-0 <73246145+kikiloveyou-0@users.noreply.github.com>
2021-11-09 17:21:08 +0100

    Ajouts majeurs
    joueurs etc

---
+++ sprites.py
@@ -0,0 +1,20 @@
+import pygame as pygame
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.allSprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.image = pygame.Surface((TILESIZE, TILESIZE))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+
+    def move(self, directionX=0, directionY=0):
+        self.x += directionX
+        self.y += directionY
+
+    def update(self):
+        self.rect.x = self.x * TILESIZE
+        self.rect.y = self.y * TILESIZE
