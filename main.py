75e2d98857cd5754aaf50aed3c3f69d052c8d268
Cromasch <73246582+Cromasch@users.noreply.github.com>
2021-11-14 16:03:42 +0100

    Plaques de pressions, niveau 1, placements caméras

--- main.py
+++ main.py
@@ -14,27 +14,37 @@ class Game:
         self.loadData()
         self.keyFound = False
 
-
     def loadData(self):
         gameFolder = path.dirname(__file__)
         self.mapData = []
         with open(path.join(gameFolder,'level1.txt'), 'rt') as f:
             for line in f:
                 self.mapData.append(line)
- 
+
     def new(self):
-        #Initialise toutes les variables et commence une nouvelle instance du jeu
+        #Initialise toutes les variables et commence une nouvelle instance du jeu mais uniquement pour le menu
         self.allSprites = pygame.sprite.Group()
         self.walls = pygame.sprite.Group()
         self.keyGroup = pygame.sprite.Group()
+        self.cameras = pygame.sprite.Group()
+        self.views = pygame.sprite.Group()
         #enumerate = return la valeur de la liste ET son index.
         for row, tiles in enumerate(self.mapData):
             for colone, tile in enumerate(tiles):
                 if tile == 'w':
                     self.wall = Wall(self,colone, row)
-                    
-                if tile == 'c':
+
+                if tile == '8':
                     self.camera = Camera(self, colone, row, "up")
+                if tile == '2':
+                    self.camera = Camera(self, colone, row, "down")
+                if tile == '6':
+                    self.camera = Camera(self, colone, row, "right")
+                if tile == '4':
+                    self.camera = Camera(self, colone, row, "left")
+
+                if tile == 'l':
+                    self.plaque = Plaque(self,colone,row)
 
                 if tile == 'k':
                     self.key = Key(self,colone,row)
@@ -48,6 +58,77 @@ class Game:
                 if tile == 'p':
                     self.player = Player(self, colone, row)
 
+        self.view1 = View(self, 9, 15)
+        self.view2 = View(self, 15, 16)
+
+        self.alive = True
+        self.gamestart = False
+        self.menu()
+
+    def respawnnew(self):
+        #Initialise toutes les variables et commence une nouvelle instance du jeu mais uniquement pour les respawns
+        self.allSprites = pygame.sprite.Group()
+        self.walls = pygame.sprite.Group()
+        self.keyGroup = pygame.sprite.Group()
+        self.cameras = pygame.sprite.Group()
+        self.views = pygame.sprite.Group()
+        #enumerate = return la valeur de la liste ET son index.
+        for row, tiles in enumerate(self.mapData):
+            for colone, tile in enumerate(tiles):
+                if tile == 'w':
+                    self.wall = Wall(self,colone, row)
+
+                if tile == '8':
+                    self.camera = Camera(self, colone, row, "up")
+                if tile == '2':
+                    self.camera = Camera(self, colone, row, "down")
+                if tile == '6':
+                    self.camera = Camera(self, colone, row, "right")
+                if tile == '4':
+                    self.camera = Camera(self, colone, row, "left")
+
+                if tile == 'l':
+                    self.plaque = Plaque(self,colone,row)
+
+                if tile == 'k':
+                    self.key = Key(self,colone,row)
+
+                if tile == 'v':
+                    self.view = View(self, colone, row)
+
+                if tile == 'd':
+                    self.door = Door(self, colone, row)
+
+                if tile == 'p':
+                    self.player = Player(self, colone, row)
+
+        self.view1 = View(self, 9, 15)
+        self.view2 = View(self, 15, 16)
+
+        self.activationCamera = True
+        self.alive = True
+        self.gamestart = True
+
+    def menu(self):
+        self.titre = Titre(self, 11, 10)
+        self.fleches = Fleches(self, 1, 0.5)
+        self.e = E(self, 8, 2.5)
+        self.noms = Noms(self, 25, 19)
+        self.pressEnter = PressEnter(self, 12.5, 12)
+
+    def menu_keys(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    print("test")
+                    self.gamestart = True
+                    game = Game()
+                    while True:
+                        game.respawnnew()
+                        game.run()
+
 
 
     def run(self):
@@ -55,12 +136,23 @@ class Game:
         #On raffraichit tant que le programme tourne
         while self.playing:
             self.dt = self.clock.tick(FPS) / 1000
-            self.mouvement()
             self.update()
             self.draw()
-            self.keySystem()
-            self.detection()
-        
+
+            if self.gamestart == False:
+                self.menu_keys()
+            # test si le jeu à commencé (si le joueur a appuyé sur entrée sur le menu), si oui l'autorise à se déplacer
+            if self.gamestart == True:
+                self.mouvement()
+                self.keySystem()
+
+                if self.activationCamera == True:
+                    self.detection()
+
+                self.death()
+                self.plaqueDetection()
+                self.desactivateCameras()
+
 
     def quit(self):
         pygame.quit()
@@ -106,21 +198,52 @@ class Game:
             pygame.sprite.spritecollide(self.player, self.keyGroup, True)
 
     def detection(self):
-        if self.view.x <= self.player.x < self.view.x1 and self.view.y <= self.player.y < self.view.y1:
+        if self.view1.x <= self.player.x < self.view1.x1 and self.view1.y <= self.player.y < self.view1.y1:
             print("detecté")
-            self.view.image.fill(RED)
-            self.camera.image = pygame.image.load('Tiles/images/Tilemap_15.png').convert_alpha()
-        pass
-    
-    def bouton_appuye(self):
-        if self.bouton1.x == self.player and self.bouton1.y == self.player.y:
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_e:
-                        print("appuye")
-                        self.bouton1.image = pygame.image.load('sprites\outon\outon_bas.png').convert_alpha()
-    pass
+            self.alive = False
 
+        if self.view2.x <= self.player.x < self.view2.x1 and self.view2.y <= self.player.y < self.view2.y1:
+            print("detecté")
+            self.alive = False
+
+    def plaqueDetection(self):
+        if self.plaque.x == self.player.x and self.plaque.y == self.player.y:
+            print("plaque pressé")
+            self.activationCamera = False
+            self.plaque.image = pygame.image.load('sprites\plaques\plaque_pressé.png').convert_alpha()
+
+
+    def desactivateCameras(self):
+        if self.activationCamera == False:
+            if self.camera.facing == "up":
+                IMAGE = pygame.image.load('sprites/camera/camera_non_detecté/camera_up.png').convert_alpha()
+            elif self.camera.facing == "down":
+                IMAGE = pygame.image.load('sprites/camera/camera_non_detecté/camera_down.png').convert_alpha()
+            elif self.camera.facing == "right":
+                IMAGE = pygame.image.load('sprites/camera/camera_non_detecté/camera_right.png').convert_alpha()
+            elif self.camera.facing == "left":
+                IMAGE = pygame.image.load('sprites/camera/camera_non_detecté/camera_left.png').convert_alpha()
+
+            self.view1.image.fill(GREEN)
+            self.view1.image.set_alpha(125)
+
+            self.view2.image.fill(GREEN)
+            self.view2.image.set_alpha(125)
+
+
+
+
+        pass
+
+
+
+    def death(self):
+        if self.alive == False:
+            print("vous êtes mort")
+            game = Game()
+            while True:
+                game.respawnnew()
+                game.run()
 
 game = Game()
 while True:
--- sprites.py
+++ sprites.py
@@ -7,26 +7,33 @@ class Player(pygame.sprite.Sprite):
         self.game = game
         self.groups = game.allSprites
         pygame.sprite.Sprite.__init__(self, self.groups)
-
-        self.image = pygame.image.load('Tiles/images/Player.png')
+        self.image = pygame.image.load('sprites/Player.png')
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
 
     def move(self, directionX=0, directionY=0):
-        if not self.collide(directionX, directionY):
+        if not self.collideWall(directionX, directionY) and not self.collideCamera(directionX, directionY):
             self.x += directionX
             self.y += directionY
 
     def update(self):
         self.rect.x = self.x * TILESIZE
         self.rect.y = self.y * TILESIZE
-    
-    def collide(self, directionX=0, directionY=0):
+
+    def collideWall(self, directionX=0, directionY=0):
         for wall in self.game.walls:
             if wall.x == self.x + directionX and wall.y == self.y + directionY:
                 return True
         return False
+
+    def collideCamera(self, directionX=0, directionY=0):
+        for camera in self.game.cameras:
+            if camera.x == self.x + directionX and camera.y == self.y + directionY:
+                return True
+        return False
+
+
 
 
 class Door(pygame.sprite.Sprite):
@@ -48,7 +55,7 @@ class Key(pygame.sprite.Sprite):
         self.groups = game.allSprites, game.keyGroup
         self.game = game
         pygame.sprite.Sprite.__init__(self, self.groups)
-        self.image = pygame.image.load('Tiles/images/Key.png')
+        self.image = pygame.image.load('sprites/cle.png')
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
@@ -59,13 +66,20 @@ class Key(pygame.sprite.Sprite):
 
 class Camera(pygame.sprite.Sprite):
     def __init__(self, game, x, y, facing):
-        # le sprite de la caméra ne s'affiche pas car je n'arrive pas à créer et faire co-exister plusieurs sprites
-        self.groups = game.allSprites
+        self.groups = game.allSprites, game.cameras
         self.game = game
         pygame.sprite.Sprite.__init__(self, self.groups)
         self.facing = facing
         if self.facing == "up":
-            IMAGE = pygame.image.load('Tiles/images/Tilemap_15.png').convert_alpha()
+            IMAGE = pygame.image.load('sprites/camera/camera_detecté/camera_up.png').convert_alpha()
+        elif self.facing == "down":
+            IMAGE = pygame.image.load('sprites/camera/camera_detecté/camera_down.png').convert_alpha()
+        elif self.facing == "right":
+            IMAGE = pygame.image.load('sprites/camera/camera_detecté/camera_right.png').convert_alpha()
+        elif self.facing == "left":
+            IMAGE = pygame.image.load('sprites/camera/camera_detecté/camera_left.png').convert_alpha()
+
+
         self.image = IMAGE
         self.rect = self.image.get_rect()
         self.x = x
@@ -78,10 +92,10 @@ class Camera(pygame.sprite.Sprite):
 class View(pygame.sprite.Sprite):
     def __init__(self, game, x, y):
         self.game = game
-        self.groups = game.allSprites
+        self.groups = game.allSprites, game.views
         pygame.sprite.Sprite.__init__(self, self.groups)
         self.image = pygame.Surface((TILESIZE*5, TILESIZE*2))
-        self.image.fill(GREEN)
+        self.image.fill(RED)
         self.image.set_alpha(125)
         self.rect = self.image.get_rect()
         self.x = x
@@ -99,7 +113,7 @@ class Wall(pygame.sprite.Sprite):
         self.groups = game.allSprites, game.walls
         pygame.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self.image = pygame.Surface((TILESIZE, TILESIZE))
+        self.image = pygame.image.load('sprites/wall.png')
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
@@ -107,32 +121,117 @@ class Wall(pygame.sprite.Sprite):
         self.rect.y = y * TILESIZE
         if self.x == 0:
             if self.y == 0:
-                self.image = pygame.image.load('Tiles/images/Tilemap_04.png')
+                self.image = pygame.image.load('sprites/murs/mur_upleft.png')
             elif self.y > ((HEIGHT / TILESIZE)-2):
-                self.image = pygame.image.load('Tiles/images/Tilemap_05.png')
+                self.image = pygame.image.load('sprites/murs/mur_downleft.png')
             else:
-                self.image = pygame.image.load('Tiles/images/Tilemap_08.png')
+                self.image = pygame.image.load('sprites/murs/mur_left.png')
         elif self.y == 0:
             if self.x == 0:
-                self.image = pygame.image.load('Tiles/images/Tilemap_04.png')
+                self.image = pygame.image.load('sprites/murs/mur_upleft.png')
             elif self.x > ((WIDTH / TILESIZE)-2):
-                self.image = pygame.image.load('Tiles/images/Tilemap_03.png')
+                self.image = pygame.image.load('sprites/murs/mur_upright.png')
             else:
-                self.image = pygame.image.load('Tiles/images/wall.png')
+                self.image = pygame.image.load('sprites/murs/mur_up.png')
         elif self.x > ((WIDTH / TILESIZE)-2):
             if self.y == 0:
-                self.image = pygame.image.load('Tiles/images/Tilemap_04.png')
+                self.image = pygame.image.load('sprites/murs/mur_upleft.png')
             elif self.y > ((HEIGHT / TILESIZE)-2):
-                self.image = pygame.image.load('Tiles/images/Tilemap_07.png')
+                self.image = pygame.image.load('sprites/murs/mur_downright.png')
             else:
-                self.image = pygame.image.load('Tiles/images/Tilemap_10.png')
+                self.image = pygame.image.load('sprites/murs/mur_right.png')
         elif self.y > ((HEIGHT / TILESIZE)-2):
             if self.x == 0:
-                self.image = pygame.image.load('Tiles/images/Tilemap_05.png')
+                self.image = pygame.image.load('sprites/murs/mur_downleft.png')
             elif self.x > ((WIDTH / TILESIZE)-2):
-                self.image = pygame.image.load('Tiles/images/Tilemap_07.png')
+                self.image = pygame.image.load('sprites/murs/mur_downright.png')
             else:
-                self.image = pygame.image.load('Tiles/images/Tilemap_09.png')
+                self.image = pygame.image.load('sprites/murs/mur_down.png')
         else:
-            self.image.fill((195,197,183))
+            self.image.fill((165,165,165))
+
+class Titre(pygame.sprite.Sprite):
+    def __init__(self, game, x, y,):
+        self.groups = game.allSprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.image = pygame.image.load('sprites\menue\zgegos.png').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+
+    def update(self):
+        self.rect.x = self.x * TILESIZE
+        self.rect.y = self.y * TILESIZE
+
+class Fleches(pygame.sprite.Sprite):
+    def __init__(self, game, x, y,):
+        self.groups = game.allSprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.image = pygame.image.load('sprites\menue\zgegis.png').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+
+    def update(self):
+        self.rect.x = self.x * TILESIZE
+        self.rect.y = self.y * TILESIZE
+
+class E(pygame.sprite.Sprite):
+    def __init__(self, game, x, y,):
+        self.groups = game.allSprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.image = pygame.image.load('sprites\menue\zgegas.png').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+
+    def update(self):
+        self.rect.x = self.x * TILESIZE
+        self.rect.y = self.y * TILESIZE
+
+class Noms(pygame.sprite.Sprite):
+    def __init__(self, game, x, y,):
+        self.groups = game.allSprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.image = pygame.image.load('sprites\menue\zgegons.png').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+
+    def update(self):
+        self.rect.x = self.x * TILESIZE
+        self.rect.y = self.y * TILESIZE
+
+class PressEnter(pygame.sprite.Sprite):
+    def __init__(self, game, x, y,):
+        self.groups = game.allSprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.image = pygame.image.load('sprites\menue\zgegus.png').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+
+    def update(self):
+        self.rect.x = self.x * TILESIZE
+        self.rect.y = self.y * TILESIZE
+
+class Plaque(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.allSprites
+        self.game = game
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.image = pygame.image.load('sprites\plaques\plaque_non_pressé.png').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+
+    def update(self):
+        self.rect.x = self.x * TILESIZE
+        self.rect.y = self.y * TILESIZE
+
+
+
+
+
+
 
